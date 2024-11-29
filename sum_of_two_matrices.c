a520ac3cb4dddbd14c88f238b393243186b67cdd
Lazy programmer <ayushgangwar8887@gmail.com>
2024-11-29 15:08:41 +0530

    Update sum_of_two_matrices.c

--- sum_of_two_matrices.c
+++ sum_of_two_matrices.c
@@ -3,32 +3,34 @@ int main(){
 	int arr1[2][2];
 	int arr2[2][2];
 	int result[2][2];
+	
 	printf("Enter elements of the first matrix:\n");
-	for(int i=1;i<2;i++){
+	for(int i=0;i<2;i++){
 		for(int j=0;j<2;j++){
-			scanf("%d ",&arr1[i][j]);
+			scanf("%d", &arr1[i][j]);
 		}
 	}
 	
 	printf("Enter elements of the second matrix:\n");
-	for(int i=1;i<2;i++){
+	for(int i=0;i<2;i++){
 		for(int j=0;j<2;j++){
-			scanf("%d ",&arr2[i][j]);	
+			scanf("%d", &arr2[i][j]);	
 		}
 	}
 	
-	for(int i=1;i<2;i++){
-		for(int j;j<2;j++){
+	for(int i=0;i<2;i++){
+		for(int j=0;j<2;j++){
 			result[i][j] = arr1[i][j] + arr2[i][j];
 		}
 	}
 	
 	printf("Sum of the matrices is:\n");
-	for(int i=1;i<2;i++){
+	for(int i=0;i<2;i++){
 		for(int j=0;j<2;j++){
 				printf("%d ",result[i][j]);
 			}
+			printf("\n");
 		}
-		printf("\n");
+		
 
 } 
